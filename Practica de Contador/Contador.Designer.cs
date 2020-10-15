3a60c25057a67d54386fab2c4331e2c39df02598
sergiobaldomerobeltran@gmail.com <sergiobaldomerobeltran@gmail.com>
2020-10-14 17:12:49 -0700

    Contador para evaluacion.

---
+++ Practica de Contador/Contador.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Practica_de_Contador
+{
+    partial class Contador
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Contador));
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Displayeador = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Add
+            // 
+            this.Add.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Add.Location = new System.Drawing.Point(18, 279);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(167, 39);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "Añadir número";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Remove.Location = new System.Drawing.Point(191, 279);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(164, 39);
+            this.Remove.TabIndex = 1;
+            this.Remove.Text = "Borrar ultimo número";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 168);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(92, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(166, 110);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Displayeador
+            // 
+            this.Displayeador.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Displayeador.Location = new System.Drawing.Point(111, 41);
+            this.Displayeador.Name = "Displayeador";
+            this.Displayeador.Size = new System.Drawing.Size(126, 29);
+            this.Displayeador.TabIndex = 5;
+            this.Displayeador.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Contador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(436, 336);
+            this.Controls.Add(this.Displayeador);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Name = "Contador";
+            this.Text = "Contador";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox Displayeador;
+    }
+}
+
---
+++ Practica de Contador/Contador.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_de_Contador
+{
+    public partial class Contador : Form
+
+    /*Realizar un programa en C# que maneje eventos, debe implementar 2 botones para que realice las siguientes acciones, 
+     * cada que se presione el primer botón aparece un número nuevo en pantalla (generado de manera consecutiva) y cada que 
+     * presione el segundo botón borre el último número generado. No puede borrar si ya no hay números, máximo va a generar 25 números.*/
+    {
+        Random rng = new Random();
+        ArrayList cadena = new ArrayList();
+
+        public void GenerarNum() {
+            if (cadena.Count == 0)
+            {
+                cadena.Add(rng.Next(1,100));
+                Imprimir();
+            }
+            else if (cadena.Count < 25) {
+                cadena.Add((int)cadena[(cadena.Count-1)] + 1);
+                Imprimir();
+            }
+        }
+
+        public void RemoverNum() {
+            if (cadena.Count > 0) {
+                cadena.RemoveAt((cadena.Count-1));
+                Imprimir();
+            }
+        }
+
+        public void Imprimir() {
+            if (cadena.Count > 0)
+            {
+                Displayeador.Text = cadena[(cadena.Count-1)].ToString();
+            }
+            else {
+                Displayeador.Text = "Vacio";
+            }
+        }
+
+        public Contador()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            GenerarNum();
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            RemoverNum();
+        }
+    }
+}
